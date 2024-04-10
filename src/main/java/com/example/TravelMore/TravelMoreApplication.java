41d0a7f79aec9cc418b8a5ee52676abdb3a4cd84
Vlad Sablin <vlads-vlads@inbox.lv>
2024-04-10 16:19:32 +0300

    Initial setup

---
+++ src/main/java/com/example/TravelMore/TravelMoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravelMore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelMoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TravelMore/TravelMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TravelMore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelMoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
