65eef99fba9cd3bdce620d33d2ce862e26049541
Vlad Sablin <vlads-vlads@inbox.lv>
2024-04-19 18:57:47 +0300

    added API calls to fetch an image cover for a trip

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchCountries();
+    window.addEventListener('load', function() {
+        const tripCards = document.querySelectorAll('.card');
+        tripCards.forEach(function(card) {
+            const tripId = card.querySelector('img').id.split('-')[1];
+            const destination = card.querySelector('.card-text').textContent.trim();
+            fetchRandomImage(destination, tripId, card);
+        });
+    });
+});
+
 function fetchCountries() {
     const url = 'https://restcountries.com/v3.1/all';
 
@@ -5,7 +17,6 @@ function fetchCountries() {
         .then(response => response.json())
         .then(data => {
             const countries = data.map(country => country.name.common);
-
             const selectDropdown = document.getElementById('tripName');
             countries.forEach(country => {
                 const option = document.createElement('option');
@@ -17,4 +28,15 @@ function fetchCountries() {
         .catch(error => console.error('Error fetching countries:', error));
 }
 
-window.onload = fetchCountries;
+function fetchRandomImage(destination, tripId, card) {
+    const imageUrl = `https://api.unsplash.com/photos/random?query=${destination}&orientation=landscape&client_id=u2wkrKKJxinMI8ocyh-Xm7euYttlDtgp3Fdg7XPFtd4`;
+
+    fetch(imageUrl)
+        .then(response => response.json())
+        .then(data => {
+            const imageSrc = data.urls.regular;
+            const tripImage = card.querySelector('img');
+            tripImage.src = imageSrc;
+        })
+        .catch(error => console.error('Error fetching random image:', error));
+}
